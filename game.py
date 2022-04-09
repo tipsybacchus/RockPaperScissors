7dd9787be1d54c7ee97fc0fd803b9d8e17814657
DJ <tipsybacchus@gmail.com>
2022-04-09 02:14:11 -0400

    A game of rock, paper, scissors with a fun twist if found.

---
+++ game.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+from ast import Break
+from random import randint
+
+#fuck cover letters
+
+def ComputerMugging():
+    while True:
+        playerChoice = input(WillSmith)
+        if playerChoice == ("Wallet"):
+            print ("Thanks Bud, good choice. *Walks away*")
+            break
+        elif playerChoice != ("Wallet"):
+            print ("I didn't ask for", playerChoice + "!" " Show me your wallet!")        
+
+
+
+
+
+
+
+
+
+
+#create a list of play options
+t = ["Rock","Paper","Scissors","Gun"]
+
+WillSmith = "Rock, Paper, Scissors?\nType your shit here: "
+
+pandoraBox = False
+
+while True:
+    #set playerChoice to True
+    playerChoice = input(WillSmith)
+    if not playerChoice in t:
+        print ("Drive off a cliff and try again")
+        continue
+       
+    computer = t[randint(0,3 if pandoraBox else 2)]
+
+    if playerChoice == computer:
+        print("Tie!")
+
+    elif computer == "Gun":
+        print("Computer shows Gun and insists you choose wallet.")
+        ComputerMugging()
+
+    elif playerChoice == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", playerChoice)
+        else:
+            print("You win!", playerChoice, "smashes", computer)
+
+    elif playerChoice == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", playerChoice)
+        else:
+            print("You win!", playerChoice, "covers", computer)
+
+    elif playerChoice == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", playerChoice)
+        else:
+            print("You win!", playerChoice, "cut", computer)
+
+    elif playerChoice == "Gun":
+        print("Computer brought", computer, "to a gun fight")
+        pandoraBox = True
+
+    else:
+        print("That didn't work! You could have typed Gun for all I care!")
